3899f6a44c21bc06a494c1639aa5c026b4821fae
TalisDuarte007 <tcd07@hotmail.com>
2022-07-07 18:29:05 -0300

    first commit

---
+++ src/app/components/bookstore-app/product-list/product-list.componente.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@angular/core";
+import {HttpClientModule, HttpHeaders} from "@angular/common/http";
+import {HttpClient} from "@angular/common/http";
+import { Book } from "./model/Book";
+
+@Injectable()
+export class BooksService{
+    private url = "https://localhost:44382/api/bookstore";
+
+    httpOptions={
+        Headers: new HttpHeaders({'content-type': "application/json"})
+    }
+
+    constructor(private http:HttpClient){}
+
+    getBook(){
+        return this.http.get(this.url)
+    }
+}
